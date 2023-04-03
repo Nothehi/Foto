84a44ad1df6bdf272afe535cb628d1f6a8bc9861
nothehi <nodehi7813@gmail.com>
2023-04-03 10:29:53 +0330

    fix python script

--- vision/main.py
+++ vision/main.py
@@ -3,16 +3,30 @@
 from PIL import Image
 import face_recognition
 import sys
+import random
+import string
+
+def randomStr(count = 40):
+    return ''.join(random.choice(string.ascii_lowercase+string.ascii_uppercase+string.digits) for i in range(count))
 
 def getFaces(source):
     image = face_recognition.load_image_file(source)
     face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="cnn")
+    faces = []
 
-    for idx, face_location in enumerate(face_locations):
+    for face_location in face_locations:
         top, right, bottom, left = face_location
-        face_image = image[top:bottom, left:right]
-        pil_image = Image.fromarray(face_image)
-        pil_image.save("p-{}.jpg".format(idx), 'JPEG')
+        filename = "faces/{}.jpg".format(randomStr())
+        Image.fromarray(image[top:bottom, left:right]).save("../storage/{}".format(filename), 'JPEG')
+        encodings = face_recognition.face_encodings(face_recognition.load_image_file("../storage/{}".format(filename)))
+
+        faces.append({
+            'image': filename,
+            'coordinate': [top, right, bottom, left],
+            'encoding': list(encodings[0]) if encodings else ""
+        })
+
+    return faces
 
 def compareFace(face, picture):
     face = face_recognition.load_image_file(face)
@@ -25,21 +39,27 @@ def compareFace(face, picture):
         face_encoding = face_recognition.face_encodings(face)[0]
         picture_encoding = face_recognition.face_encodings(picture)
     except IndexError:
-        print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
-        quit()
+        # print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+        # quit()
+        return False
 
     known_faces = [
         face_encoding,
     ]
 
+
     # results is an array of True/False telling if the unknown face matched anyone in the known_faces array
     # return face_recognition.compare_faces(known_faces, picture_encoding)
     for unknown_face in picture_encoding:
+        # print(face_recognition.face_distance(known_faces, unknown_face))
         if (face_recognition.compare_faces(known_faces, unknown_face)[0]):
             return True
 
     return False
 
 
-# getFaces(sys.argv[1])
-print(compareFace(sys.argv[1], sys.argv[2]))
+match sys.argv[1]:
+    case 'compare':
+        print(compareFace(sys.argv[2], sys.argv[3]))
+    case 'face':
+        print(getFaces(sys.argv[2]))
